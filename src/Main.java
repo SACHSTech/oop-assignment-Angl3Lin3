70e0863fe7fbc000553e7316100853fa1ae35b52
Angl3Lin3 <angle.lin23@ycsdbk12.ca>
2022-11-14 10:59:18 -0500

    Added Item input in Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,15 +17,21 @@ public class Main {
 
         addLine();
         System.out.println(start);
+        addLine();
+
+        HashMap<String, Boolean> itemInfo = new HashMap<String, Boolean>();
 
         for(int i = 0; i < intItemNum; i++){
             System.out.print("Enter item name: ");
             String itemName = scan.next();
-            System.out.print("Enter true: if you can use the item, false: if you can not.");
+            System.out.print("Enter true: if you can use the item, false: if you can not: ");
             boolean itemUse = scan.nextBoolean();
             addLine();
 
             Items create = new Items(itemName, itemUse);
+            System.out.println(create);
+            itemInfo.put(itemName, itemUse);
+            addLine();
         }
 
         scan.close();
