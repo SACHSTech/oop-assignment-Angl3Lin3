9f3840e142077c7276787f6eb0a0e3f51585362d
Angl3Lin3 <angle.lin23@ycsdbk12.ca>
2022-11-15 16:45:03 -0500

    Added getAttackType method to Attacks class

--- src/Attacks.java
+++ src/Attacks.java
@@ -2,6 +2,9 @@ public class Attacks {
     
     private String strAttackType;
     private int intAttackDamage = 5;
-    
+
+    public String getAttackType(){
+        return strAttackType;
+    }
 
 }
