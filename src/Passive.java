5b53b0eed60c2360cacf338d5a2f165b8a9eeac5
Angl3Lin3 <angle.lin23@ycsdbk12.ca>
2022-11-15 16:38:50 -0500

    Added Passive method to Passive class

--- src/Passive.java
+++ src/Passive.java
@@ -1,9 +1,13 @@
-public class Passive {
+public class Passive extends Entity{
     
     private boolean isAttacked = false;
 
     public boolean getAttacked(){
         return isAttacked;
+    }
+
+    public Passive(String strName, String strNoise){
+        super(strName, strNoise);
     }
 
     public String toString(){
