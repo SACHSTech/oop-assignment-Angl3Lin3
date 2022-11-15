3a73c6ed40fd2bb4a68c5b0ded40737bcc3d0797
Angl3Lin3 <angle.lin23@ycsdbk12.ca>
2022-11-15 16:50:50 -0500

    Added getDropName method to Drops class

--- src/Drops.java
+++ src/Drops.java
@@ -3,6 +3,9 @@ public class Drops {
     private String strDropName;
     private String strDropType;
     private int intDropNum;
-    
+
+    public String getDropName(){
+        return strDropName;
+    }
 
 }
