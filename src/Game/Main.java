861d5e1a5228ff510c1efc063126b7fb32a748fc
Angl3Lin3 <angle.lin23@ycsdbk12.ca>
2022-11-18 15:59:52 -0500

    Changes in Main class

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void start(String[] args) throws InterruptedException {
         
         addLine();
         //Initialize
