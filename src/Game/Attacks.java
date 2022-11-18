4b2264c9e4dcda4445f4473440eb2a7b67fe3959
Angl3Lin3 <angle.lin23@ycsdbk12.ca>
2022-11-18 12:14:26 -0500

    Adding Attack class javadoc Comments

--- src/Game/Attacks.java
+++ src/Game/Attacks.java
@@ -1,21 +1,51 @@
 package Game;
+
+/**
+ * Attacks class file
+ * @author: Angel Lin
+ * 
+ */
+
 public class Attacks {
     
+    //Instance variables
     private String strAttackType = "stabs";
     private int intAttackDamage = 5;
 
+    /**
+     * Returns the AttackType of an Attacks object
+     * 
+     * @return String getAttackType which represents the type of attack for the hostile entity
+     */
     public String getAttackType(){
         return strAttackType;
     }
 
+    /**
+     * Returns the AttackDamage of an Attacks object
+     * 
+     * @return int getAttackDamage which represents the attack damage of the hostile entity
+     */
     public int getAttackDamage(){
         return intAttackDamage;
     }
 
+    /**
+     * Return the toString of an Attacks object
+     * 
+     * @return String toString which returns a message of the entity attacking the player
+     */
     public String toString(){
         return Entity.getEntityName() + " attacks you by " + strAttackType + " for " + intAttackDamage;
     }
 
+    /**
+     * 
+     * Constructor - creates new instance of Attacks object
+     * 
+     * Has no parameters
+     * 
+     */
     public Attacks(){
     }
 
